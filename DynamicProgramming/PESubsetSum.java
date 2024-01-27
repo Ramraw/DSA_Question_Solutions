cfcef71ef91f120d4cdc5bd8d1178adeb980bf7d
seema yadav <ramvijayya2004@gmail.com>
2024-01-27 10:32:53 +0530

    first commit

---
+++ DynamicProgramming/PESubsetSum.java
@@ -0,0 +1,39 @@
+class Solution {
+    static int equalPartition(int N, int arr[]) {
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += arr[i];
+        }
+
+        // Check if the total sum is odd, then it cannot be partitioned into two equal subsets
+        if (sum % 2 != 0) {
+            return 0;
+        }
+
+        // Check if there exists a subset with sum equal to sum/2
+        return subSetSum(arr, sum / 2, N) ? 1 : 0;
+    }
+
+    static boolean subSetSum(int[] arr, int sum, int n) {
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < sum + 1; j++) {
+                if (i == 0)
+                    dp[i][j] = false;
+                if (j == 0)
+                    dp[i][j] = true;
+            }
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (arr[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+}
