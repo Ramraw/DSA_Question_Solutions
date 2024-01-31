097f66311e546dd30cbf23fd30ee4d98c5a00ae4
seema yadav <ramvijayya2004@gmail.com>
2024-01-31 12:15:52 +0530

    first commit

---
+++ BinaryTree/Height.java
@@ -0,0 +1,28 @@
+/* 
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+ */
+
+class Solution {
+    //Function to find the height of a binary tree.
+    int height(Node node) 
+    {
+        if(node == null) {
+            return 0;
+        }
+        
+        int lh = height(node.left);
+        int rh = height(node.right);
+        
+        return Math.max(lh, rh) + 1;
+    }
+}
